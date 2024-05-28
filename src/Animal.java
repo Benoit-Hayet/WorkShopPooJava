1495d9e9a27ea90619e974097fc1134bc5be679d
Benoit-Hayet <hayetbenoit@gmail.com>
2024-05-28 16:39:40 +0200

    End

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class Animal {
-}
+public class Animal {
+
+    private String name;
+    private int size;
+    private boolean isCarnivorous = false;
+    private int pawsNbr;
+
+    public Animal (String name,int size, boolean isCarnivorous, int pawsNbr){
+        this.name = name;
+        this.size = size;
+        this.isCarnivorous = isCarnivorous;
+        this.pawsNbr = pawsNbr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public boolean isCarnivorous() {
+        return isCarnivorous;
+    }
+
+    public void setCarnivorous(boolean carnivorous) {
+        isCarnivorous = carnivorous;
+    }
+
+    public int getPawsNbr() {
+        return this.pawsNbr;
+    }
+
+    public void setPawsNbr(int pawsNbr) {
+        this.pawsNbr = pawsNbr;
+    }
+
+    public String introduce() {
+        String statutCarnivorous = isCarnivorous?"Carnivorous":"Not Carnivorous";
+       return "Hey, en tant que " + getName() + ", j’ai " + getPawsNbr() + " pattes et je suis " + statutCarnivorous;
+
+    }
+
+    public String isDangerous() {
+        if (getSize() > 50 && isCarnivorous()) {
+            return "Je suis dangereux";
+        } else {
+            return "Je ne suis pas dangereux";
+        }
+    }
+    }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.w3c.dom.ls.LSOutput;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        Animal lion = new Animal ("Lion",51,true,4);
+        Animal parrots = new Animal ("Perroquet",7,false,2);
+
+        System.out.println(lion.introduce()+" "+lion.isDangerous());
+        System.out.println(parrots.introduce()+" "+parrots.isDangerous());
+
         }
-    }
 }
